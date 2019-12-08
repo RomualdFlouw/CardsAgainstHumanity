061bb7e7ef10bca10f945c8cf19ab09870c9430e
DarJasper <jasper.dar@student.howest.be>
2019-12-08 17:58:38 +0100

    Integrated hub

--- cah_frontend/src/components/Lobby.vue
+++ cah_frontend/src/components/Lobby.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="lobby">
     <div >
-        <lobbymembers  v-for="p in players" :key="p.id" :username="p.username" :isReady="p.isReady"/>
+        <lobbymembers  v-for="p in players" :key="p.id" :username="p.name" :isReady="p.readyState"/>
     </div>
     <input v-show="!isReady" v-on:click="getReady()" class="button" type="submit" value="Ready" >
     <h2 v-show="isReady">waiting for everyone to get ready...</h2>
@@ -17,15 +17,9 @@ export default {
     name:'lobby',
     data(){
         return{
-            players:[
-                {username: "jasper", isReady: false},
-                {username: "Romuald", isReady: true},
-                {username: "kevin", isReady: true},
-                {username: "ayron", isReady: true},
-                {username: "thibault", isReady: true},
-                {username: "debrycke", isReady: false},
-            ],
-            isReady: false
+            players:[],
+            isReady: false,
+            gameStarting: false
         }
     },
     props:{
@@ -36,13 +30,42 @@ export default {
     },
     methods:{
         getReady: function(){
+            //this.$router.push({ path: `/game/${this.nickname}` })
+            if (this.gameStarting){
+                return;
+            };
+            this.$lobbyHub.ClientReadyStateChange({Name: this.nickname, ReadyState: !this.isReady});
+        },
+        UpdateLobby: function(users){
+            console.log(users);
+            this.players = users;
+        },
+        UpdateReadyState: function(){
             this.isReady = !this.isReady;
             localStorage.setItem('readyState', this.isReady);
-            this.$router.push({ path: `/game/${this.nickname}` })
-      }
+        },
+        StartCountdownToGame: function(){
+            console.log("STARTING COUNTDOWN TO GAME");
+            this.gameStarting = true;
+            //TODO ADD COUNTDOWN TIMER
+            //TODO DISABLE BUTTON
+        },
+        StopCountdownToGame: function(){
+            console.log("STOPPING COUNTDOWN TO GAME");
+            this.gameStarting = false;
+            //TODO ADD COUNTDOWN TIMER
+            //TODO DISABLE BUTTON
+        }
+
+
     },
     created(){
         localStorage.setItem('readyState', false);
+        this.$lobbyHub.$on("update-lobby", this.UpdateLobby);
+        this.$lobbyHub.$on("update-ready-state", this.UpdateReadyState)
+        this.$lobbyHub.$on("start-countdown-game", this.StartCountdownToGame)
+        this.$lobbyHub.$on("stop-countdown-game", this.StopCountdownToGame)
+        this.$lobbyHub.GetAllPlayers();
     }
 }
 </script>
--- cah_frontend/src/views/Login.vue
+++ cah_frontend/src/views/Login.vue
@@ -1,34 +1,60 @@
 <template>
-    <div class="login">
-        <h1 class="logo">Cards Against Humanity</h1>
-        <div class="content">
-            <input  v-model="nickname" class="login_input" type="text" placeholder="Type your nickname" id="nickname">    
-            <input v-on:click="login(nickname)" class="button" type="submit" value="Send" >
-        </div>
+  <div class="login">
+    <h1 class="logo">Cards Against Humanity</h1>
+    <div class="content">
+      <input
+        v-model="nickname"
+        class="login_input"
+        type="text"
+        placeholder="Type your nickname"
+        id="nickname"
+      />
+      <input
+        v-on:click="login(nickname)"
+        class="button"
+        type="submit"
+        value="Send"
+      />
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'login',
-  data(){
-      return{
-        nickname: null,
-      }
+  name: "login",
+  data() {
+    return {
+      nickname: null
+    };
   },
-  components: {
-      
-  },
-  methods:{
-      login: function(nickname){
-          console.log(nickname);
-          if(nickname == null){
-              console.log("is empty");
-          }else{
-              this.$store.dispatch("loginToken", {UserName : nickname});
-              this.$router.push({ path: `/home/${nickname}` }) // -> /user/123
-          }
+  components: {},
+  methods: {
+    login: function(nickname) {
+      console.log(nickname);
+      if (nickname == null || nickname == "") {
+        console.log("is empty");
+      } else {
+        this.$lobbyHub.LoginUser({ Name: nickname, ReadyState: false });
       }
+    },
+    UsernameChecked: function(isAvailable){
+      if (isAvailable) {
+        this.$router.push({ path: `/home/${this.nickname}` }); // -> /user/123
+      } else {
+        console.log("username already taken");
+        // TODO 
+        // WRITE CODE TO DISPLAY THAT USERNAME IS TAKEN ON THE WEBSITE
+      }
+    },
+    LobbyFull: function(){
+      console.log("Lobby is full");
+    }
+
+  },
+  created() {
+    this.$store.dispatch("loginToken");
+    this.$lobbyHub.$on("username-checked", this.UsernameChecked);
+    this.$lobbyHub.$on("lobby-full", this.LobbyFull);
   }
-}
+};
 </script>
