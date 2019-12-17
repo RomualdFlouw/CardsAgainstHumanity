6fecf4a8cae321b2471a23c7325f103c5a077441
RomualdFlouw <romuald.flouw@student.howest.be>
2019-12-17 20:30:50 +0100

    settings_finished

--- client/src/views/Settings.vue
+++ client/src/views/Settings.vue
@@ -5,11 +5,11 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="#FF0000" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
             </router-link>
         </div>
-      <h2 class="logo">{{$t("SETTINGS")}}</h2>
+      <h1 class="logo">{{$t("SETTINGS")}}</h1>
     <div class="content">
-        <select name="LeaveType" @change="onChange($event)" class="form-control" v-model="key">
-            <option value="en">english</option>
-            <option value="nl">nederlands</option>
+        <select class="settings_select" name="LeaveType" @change="onChange($event)" v-model="key">
+            <option class="settings_select__child" value="en">english</option>
+            <option class="settings_select__child" value="nl">nederlands</option>
         </select> 
     </div>
     </div>
@@ -37,3 +37,16 @@ export default {
   created() {},
 }
 </script>
+
+<style scoped>
+.settings_select{
+  width: 80%;
+  margin: auto;
+  max-width: 320px;
+
+  background-color: transparent;
+  border: 2px solid white;
+  color: white
+}
+
+</style>
