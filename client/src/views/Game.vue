59999da6ad372107b11c6c13aeda7694f5d857b0
RomualdFlouw <romuald.flouw@student.howest.be>
2019-12-18 10:34:58 +0100

    v-show finished

--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -4,8 +4,35 @@
             <div class="header_logout" v-on:click="logoutFunction()">
                 <svg class="" xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><path d="M16 17l5-5-5-5M19.8 12H9M10 3H4v18h6"/></svg>
             </div>
+        </div>        
+        <div v-show="cardCzar" class="game_content">
+            <div class="game_content__information">
+                <div class="information_top">
+                    <h3>{{$t("GAME_POINTS")}}{{currentPoints}}</h3>
+                </div>
+                <h2 class="information_player">{{currentChooser}} {{$t("GAME_CZAR")}}</h2>
+                <h3 v-show="IsRoundStarting">{{$t("GAME_TIME")}} {{roundVisualCounter}}</h3>
+                <h3 v-show="IsChooserStarting">{{$t("GAME_TIME")}} {{chooserVisualCounter}}</h3>
+            </div>
+            <div class="game_content__placement">
+                <div class="placement_black">
+                    <h3 class="cards_title_white">{{blackCardValue}}</h3>
+                </div>
+                <div v-show="!chosenCard" class="placement_dotted">
+                    <h3 class="cards_title_white">other players are choosing cards, as soon as the cards appear choose your favorite and submit.</h3>
+                </div>                
+                <div v-show="chosenCard" class="placement_white">
+                    <h3 class="cards_title_black">{{chosenCardText}}</h3>
+                </div>
+            </div>
+            <input v-show="chosenCard" v-on:click="sendChosenCardCzar(chosenCard)" class="game_content__submit" type="submit" :value="`${this.$t('GAME_CONFIRM')}`"> 
+            <div class="game_content__cards">
+                <div class="cards_container">
+                    <cards @clicked="onClickChild" v-for="c in Playercards" :key="c.id"  :card="c" />
+                </div>
+            </div>
         </div>
-        <div class="game_content">
+        <div v-show="!cardCzar" class="game_content">
             <div class="game_content__information">
                 <div class="information_top">
                     <h3>{{$t("GAME_POINTS")}}{{currentPoints}}</h3>
@@ -28,7 +55,7 @@
             <input v-show="chosenCard" v-on:click="sendChosenCard(chosenCard)" class="game_content__submit" type="submit" :value="`${this.$t('GAME_CONFIRM')}`"> 
             <div class="game_content__cards">
                 <div class="cards_container">
-                    <cards @clicked="onClickChild" v-for="c in Playercards" :key="c.id"  :card="c" />
+                    <cards @clicked="onClickChild" v-for="c in ToChooseCards" :key="c.id"  :card="c" />
                 </div>
             </div>
         </div>
@@ -43,19 +70,9 @@ export default {
         return{
             currentPoints: 0,
             currentChooser: "",
-
-            blackCardValue: "What is the one thing obama is good at? ___",
+            cardCzar: false,
+            blackCardValue: "",
             Playercards:[
-                {cardText: "this is the text dzada sd asdazdasd azd ", cardIndex:0},
-                {cardText: "this is the text", cardIndex:1},
-                {cardText: "this is the text", cardIndex:2},
-                {cardText: "this is the text", cardIndex:3},
-                {cardText: "this is the text", cardIndex:4},
-                {cardText: "this is the text", cardIndex:5},
-                {cardText: "this is the text", cardIndex:6},
-                {cardText: "this is the text", cardIndex:7},
-                {cardText: "this is the text", cardIndex:8},
-                {cardText: "this is the text", cardIndex:9}
             ],
             ToChooseCards:[],
 
@@ -90,6 +107,12 @@ export default {
             //doorsturen naar backend
         },
         sendChosenCard: function (value){
+            console.log("Selected Card")
+            console.log(value);
+            this.$gameHub.ClientSelectedCard(value);
+
+        },
+        sendChosenCardCzar: function (value){
             console.log("Selected Card")
             console.log(value);
             this.$gameHub.ClientSelectedCard(value);
